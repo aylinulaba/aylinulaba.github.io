749e6d18d6cd2501d62e437450d5e26df8e3ddcf
Aylin <81568176+aylinulaba@users.noreply.github.com>
2023-02-23 04:32:51 +0300

    Final build

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Portfolio } from "./Portfolio";
-export { default as Branding } from "./Branding";
-export { default as Frontend } from "./Frontend";
-export { default as Contact } from "./Contact";
